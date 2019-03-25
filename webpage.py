5f6ddd8c32018d825021d9c431b1551ac0accc8d
bobjoe400 <cnamattern@gmail.com>
2019-03-24 21:36:04 -0400

    asdfasdfasdf

--- webpage.py
+++ webpage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import cgi
 from create import main
+from lib.data_table import generate_html_with_table as ghwt
 from bs4 import BeautifulSoup as BS
 
 soup = BS(open("index.html"),'html.parser')
@@ -15,5 +16,5 @@ soup.body.append(str(main("TailNumbers.eml")))
 with open('index.html','w') as outf:
     outf.write(str(soup))
 
-print(soup) 
+print(soup)
 
